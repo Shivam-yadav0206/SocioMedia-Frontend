220bded06bbbdd3f5e401faed2a4896a9cec121a
Shivam Yadav <sy7176442@gmail.com>
2023-09-21 03:05:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { useMemo } from 'react';
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
+//import state from 'state';
+
+function App() {
+  const mode = useSelector((state)=>state.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes> 
+            <Route path='/home' element={<HomePage />}/>
+            <Route path='/' element={<LoginPage />}/>  
+            <Route path='/profile/:userId' element={<ProfilePage />}/>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
